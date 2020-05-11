4a8791620e45e8b994519fb3306be7f1cadbcb26
rafaeldtx <rafael991_@hotmail.com>
2020-05-11 10:09:32 -0300

    (Account) Fix uniqueness validation

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,5 +3,5 @@ class Account < ApplicationRecord
   has_many :account_destinations, class_name: 'Transaction', foreign_key: :account_destination_id
 
   validates :name, :amount, :token, presence: true
-  validates :number, :token, uniqueness: true
+  validates_uniqueness_of :number, :token, case_sensitive: true
 end
