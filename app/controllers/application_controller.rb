104a43dc56598451a06111eef704c337587db996
rafaeldtx <rafael991_@hotmail.com>
2020-05-13 11:30:45 -0300

    (Auth) Methods for auth httpToken in AppController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
+include ActionController::HttpAuthentication::Token::ControllerMethods
+
 class ApplicationController < ActionController::API
+  private
+
+  def token_authentication
+    authenticate_with_http_token do |token, _options|
+      Account.find_by(token: token).present?
+    end
+  end
+
+  def is_token_authenticable?
+    return true if token_authentication
+
+    render json: {
+      error: 'Acesso negado! Informe um Token válido!'
+    }, status: :unauthorized
+  end
 end
